2a12b5eb507b1596ba0ac688c5cf8dd7bcff47fc
Eric Liang <jklelxy@gmail.com>
2018-04-09 13:16:59 -0500

    added

--- src/165_CompareVersionNumbers/Solution.cpp
+++ src/165_CompareVersionNumbers/Solution.cpp
@@ -4,72 +4,68 @@
 
 #include <leetcode.h>
 
-class Solution {
-public:
-    int compareVersion(string version1, string version2) {
-        vector<int>v1 = toVector(version1);
-        vector<int>v2 = toVector(version2);
-
-        int i = 0;
-        while (i < v1.size() || i < v2.size()){
-            if (i >= v1.size()){
-                return -1;
-            }
-            if (i >= v2.size()){
-                return 1;
-            }
-
-            if (v1[i] > v2[i]){
-                return 1;
-            }
-            else if (v1[i] < v2[i]){
-                return -1;
-            }
-            else {
-                i++;
-            }
+vector<int> toVector(const string& s){
+    vector<int>res;
+    int i = 0;
+    int cnt = 0;
+    while (i < s.size()){
+        if (s[i+cnt] == '.'){
+            res.push_back(atoi(s.substr(i, cnt).c_str()));
+            i += cnt + 1;
+            cnt = 0;
         }
-        return 0;
+        else if (i+cnt >= s.size()) {
+            res.push_back(atoi(s.substr(i, cnt).c_str()));
+            break;
+        }
+        cnt++;
+    }
+    for (int j = res.size() - 1; j > 0; j--){
+        if (res[j] ==0){
+            res.pop_back();
+        }
+        else {
+            break;
+        }
     }
 
-    vector<int> toVector(const string& s){
-        vector<int>res;
-        int i = 0;
-        int cnt = 0;
-        while (i < s.size()){
-            if (s[i+cnt] == '.'){
-                res.push_back(atoi(s.substr(i, cnt).c_str()));
-                i += cnt + 1;
-                cnt = 0;
-            }
-            else if (i+cnt >= s.size()) {
-                res.push_back(atoi(s.substr(i, cnt).c_str()));
-                break;
-            }
-            cnt++;
+    return res;
+}
+
+int compareVersion(string version1, string version2) {
+    vector<int>v1 = toVector(version1);
+    vector<int>v2 = toVector(version2);
+
+    int i = 0;
+    while (i < v1.size() || i < v2.size()){
+        if (i >= v1.size()){
+            return -1;
         }
-        for (int j = res.size() - 1; j > 0; j--){
-            if (res[j] ==0){
-                res.pop_back();
-            }
-            else {
-                break;
-            }
+        if (i >= v2.size()){
+            return 1;
         }
 
-        return res;
+        if (v1[i] > v2[i]){
+            return 1;
+        }
+        else if (v1[i] < v2[i]){
+            return -1;
+        }
+        else {
+            i++;
+        }
     }
-};
+    return 0;
+}
 
 int main(){
-    Solution s;
-    auto val1 = s.compareVersion("1","1");
-    auto val2 = s.compareVersion("1","2");
-    auto val3 = s.compareVersion("1.1","1.2");
-    auto val4 = s.compareVersion("1.1","1.1");
-    auto val5 = s.compareVersion("1.2.3","1.3.4");
-    auto val6 = s.compareVersion("2.3.5","1.5.6");
-    auto val7 = s.compareVersion("1.0.0.0","1");
-    auto val8 = s.compareVersion("3.0.4.10","3.0.4.2");
+    auto val1 = compareVersion("1","1");
+    auto val2 = compareVersion("1","2");
+    auto val3 = compareVersion("1.1","1.2");
+    auto val4 = compareVersion("1.1","1.1");
+    auto val5 = compareVersion("1.2.3","1.3.4");
+    auto val6 = compareVersion("2.3.5","1.5.6");
+    auto val7 = compareVersion("1.0.0.0","1");
+    auto val8 = compareVersion("3.0.4.10","3.0.4.2");
 }
 
--- src/221_MaximalSquare/Solution.cpp
+++ src/221_MaximalSquare/Solution.cpp
@@ -4,40 +4,36 @@
 
 #include <leetcode.h>
 
-class Solution {
-public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        if (matrix.size() == 0) return 0;
+int maximalSquare(vector<vector<char>>& matrix) {
+    if (matrix.size() == 0) return 0;
 
-        int res = 0;
-        int prev = 0;
-        vector<int> dp(matrix[0].size());
-        for (int i = 0; i < matrix[0].size(); i++){
-            dp[i] = matrix[0][i] - '0';
-            res = max(res, dp[i]);
-        }
-
-        for (int row = 1; row < matrix.size(); row++){
-            prev = dp[0];
-            dp[0] = matrix[row][0] - '0';
-
-            res = max(res, dp[0]);
-
-            for (int col = 1; col < matrix[row].size(); col++){
-                int temp = dp[col];
-                int cur = matrix[row][col] - '0' == 1 ? min(min(prev, dp[col - 1]), dp[col]) + 1 : 0;
-                dp[col] = cur;
-                prev = temp;
-                res = max(res, cur);
-            }
-
-        }
-        return res * res;
+    int res = 0;
+    int prev = 0;
+    vector<int> dp(matrix[0].size());
+    for (int i = 0; i < matrix[0].size(); i++){
+        dp[i] = matrix[0][i] - '0';
+        res = max(res, dp[i]);
     }
-};
+
+    for (int row = 1; row < matrix.size(); row++){
+        prev = dp[0];
+        dp[0] = matrix[row][0] - '0';
+
+        res = max(res, dp[0]);
+
+        for (int col = 1; col < matrix[row].size(); col++){
+            int temp = dp[col];
+            int cur = matrix[row][col] - '0' == 1 ? min(min(prev, dp[col - 1]), dp[col]) + 1 : 0;
+            dp[col] = cur;
+            prev = temp;
+            res = max(res, cur);
+        }
+
+    }
+    return res * res;
+}
 
 int main(){
-    Solution s;
     vector<vector<char>> v = {{'1','0','1','0'},{'1','0','1','1'},{'1','0','1','1'},{'1','1','1','1'}};
-    auto val1 = s.maximalSquare(v);
+    auto val1 = maximalSquare(v);
 }
